36c9685a0e080f690d4fe7aad596dc0bdc212252
Ronihe <herongrong2011@gmail.com>
2019-01-21 09:08:28 -0800

    linked list

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -1,3 +1,96 @@
+//node class, each node has a value and has a pointer to the next
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class linkList {
+  constructor(val) {
+    this.head = null;
+    (this.tail = null), (this.length = 0);
+  }
+
+  // Insert
+  // push , add a new node to the end of the linked List
+  //1. accpet a value, create a new code and pass it here
+  // if there is no head, set head and tail to be the newly create node
+  // otherwise set the next property of the tail to be the new node to be the newly created one
+  // increment the length by one
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // remove
+  // pop: remove the node from the end
+  // if the lst is empty, return undefined
+  // loop through the list get the previous node for tail and make it the new tail
+  //decrement the length by 1
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return this;
+  }
+
+  //shift: remove the new noe from the beginning of the linked list
+  // if there is not nodes, return undefined
+  //store the current head property in a varaible;
+  // get the head.next, set the head.next as the new head,
+  // decrement the length by one
+  shift() {
+    if (!this.head) return undefined;
+    this.head = this.head.next;
+    this.length--;
+  }
+  //unshift: accept a value and set it as the new head
+  // create a new node and pass it to the function
+  // if there is no head property on the list. set the head and tail to the property and length to be one
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length = 1;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+const testLinkedList = new linkList();
+testLinkedList.push('andrew');
+testLinkedList.push('roni');
+testLinkedList.push('andrew');
+console.log(testLinkedList);
+testLinkedList.shift();
+console.log('shifted', testLinkedList);
+
 // Access
 
 // Search
---
+++ linkedList/linkedList.md
@@ -0,0 +1,16 @@
+# Definiteion
+
+### data structure with head, tail and length property
+
+### consists of nodes and each node has a **value** and a pointer to another node or null
+
+- Linked List
+
+  - Do not have indexes!
+  - Connected via nodes with a next pointer
+  - Random access is not allowed
+
+- Arrays
+  - Indexed in order!
+  - Insertion and deletion can be expensive
+  - Can quickly be accessed at a specific index
