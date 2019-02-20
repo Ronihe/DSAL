cd551c11a4457c8157aad3c753ed2c071a1e6dbc
Ronihe <herongrong2011@gmail.com>
2019-02-20 12:58:17 -0800

    maxdepth is done

--- dsa-trees/dsa-trees/maxDepth.js
+++ dsa-trees/dsa-trees/maxDepth.js
@@ -1,5 +1,19 @@
-function maxDepth() {
+function maxDepth(rootNode = null) {
+  if (rootNode === null) return 0;
+  //only root tree
+  if (!(rootNode.left && rootNode.right)) return 1;
 
+  // only left node
+
+  if (rootNode.left && !rootNode.right) return maxDepth(rootNode.left) + 1;
+
+  //only right  node
+  if (!rootNode.left && rootNode.right) return maxDepth(rootNode.right) + 1;
+
+  // both left and right
+
+  if (rootNode.left && rootNode.right)
+    return Math.max(maxDepth(rootNode.left), maxDepth(rootNode.right)) + 1;
 }
 
 module.exports = maxDepth;
