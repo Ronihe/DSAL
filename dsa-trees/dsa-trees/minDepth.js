d04df7695b91ed5346df0e40628dfcb84fff3dd4
Ronihe <herongrong2011@gmail.com>
2019-02-20 12:55:03 -0800

    mindepth is done

--- dsa-trees/dsa-trees/minDepth.js
+++ dsa-trees/dsa-trees/minDepth.js
@@ -1,5 +1,20 @@
-function minDepth() {
+function minDepth(rootNode = null) {
+  //empty tree
+  if (rootNode === null) return 0;
+  //only root tree
+  if (!(rootNode.left && rootNode.right)) return 1;
 
+  // only left node
+
+  if (rootNode.left && !rootNode.right) return minDepth(rootNode.left) + 1;
+
+  //only right  node
+  if (!rootNode.left && rootNode.right) return minDepth(rootNode.right) + 1;
+
+  // both left and right
+
+  if (rootNode.left && rootNode.right)
+    return Math.min(minDepth(rootNode.left), minDepth(rootNode.right)) + 1;
 }
 
 module.exports = minDepth;
