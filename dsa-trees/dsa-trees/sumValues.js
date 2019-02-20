aa5af00dcfbf6626f81735ab8433eaa3bfcbf03c
Ronihe <herongrong2011@gmail.com>
2019-02-20 12:01:09 -0800

    sumValues

--- dsa-trees/dsa-trees/sumValues.js
+++ dsa-trees/dsa-trees/sumValues.js
@@ -1,5 +1,28 @@
-function sumValues() {
+class Node {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+}
+class Tree {
+  constructor() {
+    this.root = null;
+  }
 
+  sumValues() {
+    if (this.root === null) return 0;
+
+    let total = 0;
+    let toVisitStack = [this.root];
+
+    while (toVisitStack.length) {
+      let currNode = toVisitStack.pop();
+
+      total += currNode.val;
+      for (let child of currNode.children) toVisitStack.push(child);
+    }
+    return total;
+  }
 }
 
-module.exports = sumValues;
+module.exports = { Tree, Node };
