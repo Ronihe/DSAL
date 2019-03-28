62846672e9239717afdd71c799fc90bd979f7b80
Ronihe <herongrong2011@gmail.com>
2019-03-28 13:45:50 -0700

    done

--- leecode/maximum-product-subarray.js
+++ leecode/maximum-product-subarray.js
@@ -2,11 +2,25 @@ function maximumProductSunarray(nums) {
   // curentLargest product is the larger of arr[i-1]*arr[i] and curentLargest*arr[i]
   //the globalLargest maximum product is the larger of curentLargest and globalLargest
   let currentLargest = nums[0];
+  let currentSmallest = nums[0];
   let globalLargest = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-    currentLargest = Math.max(nums[i - 1] * nums[i], currentLargest);
+    currentLargest = Math.max(
+      currentLargest * nums[i],
+      currentSmallest * nums[i],
+      nums[i]
+    );
+    currentSmallest = Math.min(
+      currentLargest * nums[i],
+      currentSmallest * nums[i],
+      nums[i]
+    );
     globalLargest = Math.max(globalLargest, currentLargest);
+    console.log(currentLargest, currentSmallest, globalLargest);
   }
   return globalLargest;
 }
+
+let test = [2, 3, -2, 4];
+console.log(maximumProductSunarray(test));
