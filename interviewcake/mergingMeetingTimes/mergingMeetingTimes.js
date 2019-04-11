cdae789bdc13107b3e70cc96270f9692152bd976
Ronihe <herongrong2011@gmail.com>
2019-04-11 14:56:06 -0700

    added solution

--- interviewcake/mergingMeetingTimes/mergingMeetingTimes.js
+++ interviewcake/mergingMeetingTimes/mergingMeetingTimes.js
@@ -39,16 +39,46 @@ function mergingMeetingTimes_V2(array) {
     let preTime = array[i - 1];
     let currTime = array[i];
 
-    if (
-      preTime.startTime < currTime.startTime &&
-      currTime.startTime < preTime.endTime
-    ) {
+    if (currTime.startTime <= preTime.endTime) {
       array[i - 1].endTime = Math.max(array[i - 1].endTime, array[i].endTime);
       array.splice(i, 1);
     }
   }
 
   return array;
+}
+
+function mergingMeetingTimes_solution(meetings) {
+  // Create a deep copy of the meetings array
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Deep_Clone
+  const meetingsCopy = JSON.parse(JSON.stringify(meetings));
+
+  // Sort by start time
+  const sortedMeetings = meetingsCopy.sort((a, b) => {
+    return a.startTime - b.startTime;
+  });
+
+  // Initialize mergedMeetings with the earliest meeting
+  const mergedMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const currentMeeting = sortedMeetings[i];
+    const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+    // If the current meeting overlaps with the last merged meeting, use the
+    // later end time of the two
+    if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+      lastMergedMeeting.endTime = Math.max(
+        lastMergedMeeting.endTime,
+        currentMeeting.endTime
+      );
+    } else {
+      // Add the current meeting since it doesn't overlap
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+
+  return mergedMeetings;
 }
 
 module.exports = mergingMeetingTimes_V2;
