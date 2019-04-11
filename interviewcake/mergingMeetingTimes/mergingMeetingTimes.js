f5f54d81e7962e104f350cb5d6f24d96ec6fd15d
Ronihe <herongrong2011@gmail.com>
2019-04-11 14:35:54 -0700

    added the the O(N(logn))

--- interviewcake/mergingMeetingTimes/mergingMeetingTimes.js
+++ interviewcake/mergingMeetingTimes/mergingMeetingTimes.js
@@ -1,6 +1,5 @@
-//brute force
-
-function mergingMeetingTimes(array) {
+//brute force BigO Time: O(N^2) Space: O(1)
+function mergingMeetingTimes_V1(array) {
   let indicator = true;
   while (indicator) {
     indicator = false;
@@ -28,4 +27,28 @@ function mergingMeetingTimes(array) {
   return array;
 }
 
-module.exports = mergingMeetingTimes;
+//better solution BigO Time: O(nlog(n)) Space: O(1)
+//sort the input the array of meetings by the startTime
+// loop throught the sorted the array
+function mergingMeetingTimes_V2(array) {
+  array.sort((a, b) => {
+    a.startTime - b.startTime;
+  });
+
+  for (let i = 1; i < array.length; i++) {
+    let preTime = array[i - 1];
+    let currTime = array[i];
+
+    if (
+      preTime.startTime < currTime.startTime &&
+      currTime.startTime < preTime.endTime
+    ) {
+      array[i - 1].endTime = Math.max(array[i - 1].endTime, array[i].endTime);
+      array.splice(i, 1);
+    }
+  }
+
+  return array;
+}
+
+module.exports = mergingMeetingTimes_V2;
